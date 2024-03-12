88133bb99f64afcba3e50b619b9ba6725bcf0a48
sunnywealthygen <163027777+sunnywealthygen@users.noreply.github.com>
2024-03-11 22:58:53 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -11,13 +11,11 @@ credentials = Credentials.from_service_account_file(
     'norse-voice-417000-0633d3d9fab6.json',
     scopes=scopes
 )
-file1 = open('thepower.txt', 'r')
-Lines = file1.readlines()
-count=0
 k=''
-for line in Lines:
-  k=line
-
+with open('thepower.txt') as file_obj: 
+    for line in file_obj: 
+        k=line
+print(k)
 
 gc = gspread.authorize(credentials)
 
@@ -27,10 +25,8 @@ worksheet.update_acell('A'+k, k)
 
 with open('thepower.txt', 'w') as file_obj:
     file_obj.write(str(int(k)+1))
-    print(str(int(k)+1))
-
+print(str(int(k)+1))
 print(k)
-file1 = open('thepower.txt', 'r')
-Lines = file1.readlines()
-for line in Lines:
-  print(k)
+with open('thepower.txt') as file_obj: 
+    for line in file_obj: 
+        print(line)
